f082d99e9612169e2a826a2180fc1aff18c7971f
Nishant-Dahake <nishantdahake73@gmail.com>
2025-05-18 22:28:42 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,8 @@
+public class Sum {
+    public static void main(String[] args) {
+        int a = 5; // example value
+        int b = 3; // example value
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+}
