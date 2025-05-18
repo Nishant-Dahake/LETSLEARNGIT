416092af78dde046df80dd4e55fdaff38837310a
Nishant-Dahake <nishantdahake73@gmail.com>
2025-05-18 22:52:10 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class multiply {
+    public static void main(String[] args) {
+        int a = 5; // You can change the value of a
+        int b = 3; // You can change the value of b
+        int result = a * b;
+        System.out.println("Multiplication of a and b: " + result);
+    }
+}
