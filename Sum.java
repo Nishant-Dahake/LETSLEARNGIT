9e99add113a465603282b049dd5638078f6d5b13
Nishant-Dahake <nishantdahake73@gmail.com>
2025-05-18 22:40:10 +0530

    added message in sum.java

--- Sum.java
+++ Sum.java
@@ -1,7 +1,7 @@
 public class Sum {
     public static void main(String[] args) {
-        int a = 5; // example value
-        int b = 3; // example value
+        int a = 5;
+        int b = 7;
         int sum = a + b;
         System.out.println("Sum: " + sum);
     }
